9aac11ce816b03c555d9c8b623f6b4eab6fe7e71
ladiajy2g <ladi.ajayi@gmail.com>
2021-04-14 15:45:58 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+
+    return(
+        <div className="large-font">
+            <h1>Hello world</h1>
+        </div>
+    )
+
+}
+
+export default App
