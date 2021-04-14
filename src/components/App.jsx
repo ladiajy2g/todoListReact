543efc8518d4d0d9d9cc7adc55f6a6578b81a3bf
ladiajy2g <ladi.ajayi@gmail.com>
2021-04-14 16:50:05 +0100

    html structure created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,24 @@ import './App.css'
 function App() {
 
     return(
-        <div className="large-font">
-            <h1>Hello world</h1>
+        <div className="app-border">
+            <div className="app-title">
+                <h3>My To-Do List</h3>
+            </div>
+            <div className="app-input">
+            <form className="row">
+            <input type="text"  placeholder="Enter Item"/>
+            <button className="btn btn-primary">Add</button>
+            </form>
+            </div>
+            <div className="app-items">
+                <ul>
+                    <li>Go to school</li>
+                    <li>Do assignment</li>
+                    <li>Visit friends</li>
+                </ul>
+            </div>
+            
         </div>
     )
 
